d32d5d5246b64bbe7176eeeead6e1017289089d7
Alvito Novandi <alvitonovandi890@gmail.com>
2024-05-27 16:28:27 +0700

    menambahkan fungsi

--- card.h
+++ card.h
@@ -22,8 +22,14 @@ typedef struct {
     Card* top;
 } dek;
 
-void initializeStack(dek* stack);
+int isEmpty(dek* stack);
 void push(dek* stack, Card card);
 Card* pop(dek* stack);
 int pengisian_nilai_kartu(Rank rank);
 Card* create_deck();
+void print_deck(Card* deck);
+void free_deck(Card* deck);
+int hitung_kartu(Card* deck);
+Card** deck_to_array(Card* deck, int size);
+void shuffle_deck(Card** array, int size);
+Card* array_to_deck(Card** array, int size);
