ae4670f6fbb3363acdf98c5eab96d1a0aa42a593
Alvito Novandi <alvitonovandi890@gmail.com>
2024-05-27 21:31:14 +0700

    menambahkan display game

---
+++ display_game.h
@@ -0,0 +1,13 @@
+#ifndef DISPLAY_GAME_H
+#define DISPLAY_GAME_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void welcome();
+void displayMenu();
+void you_win();
+void you_lose();
+void draw();
+
+#endif //DISPLAY_GAME_H
