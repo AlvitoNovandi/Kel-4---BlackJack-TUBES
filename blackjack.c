fedb7cf576f1b3413a310fdc99f2f0fd0e9be259
Muhamad Haikal Hariyanto <haikaluhuy123@gmail.com>
2024-05-28 08:11:15 +0700

    push

--- blackjack.c
+++ blackjack.c
@@ -145,6 +145,7 @@ void playBlackjack(dek* stack) {
                 if (total_value(player.hand) > 21) {
                     printf("You bust!\n");
                     you_lose();
+                    handleLoss(player);
                     winstreak = 0; // Reset winstreak on loss
                     break;
                 }
@@ -162,15 +163,20 @@ void playBlackjack(dek* stack) {
 
             if (total_value(dealer.hand) > 21) {
                 printf("Dealer busts! You win!\n");
+                
                 you_win();
+                handleWin(player, 10);
                 saveScoreToFile(&player); // Menyimpan skor pemain saat menang
             } else if (total_value(player.hand) > total_value(dealer.hand)) {
                 printf("You win!\n");
                 you_win();
+                handleWin(player, 10);
                 saveScoreToFile(&player); // Menyimpan skor pemain saat menang
             } else if (total_value(player.hand) < total_value(dealer.hand)) {
                 printf("You lose!\n");
+                
                 you_lose();
+                handleLoss(player);
                 winstreak = 0; // Reset winstreak on loss
             } else {
                 printf("It's a tie!\n");
